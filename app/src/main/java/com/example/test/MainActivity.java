67ab80e5712ce799441f51413ccc354d3e245e2b
haa741123 <66108236+haa741123@users.noreply.github.com>
2024-06-01 12:03:53 +0900

    바텀시트 테스트 베타

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.test;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -23,6 +27,10 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
     WebView webview;
@@ -40,11 +48,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         webview = findViewById(R.id.activity_main_web);
+        textview = findViewById(R.id.textView);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // 관심지역을 설정해주세요 클릭 시 바텀시트 띄우기
+        TextView textView3 = findViewById(R.id.location);
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyBottomSheetDialogFragment bottomSheetDialogFragment = new MyBottomSheetDialogFragment();
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        else {
+        } else {
             setWebView();
         }
     }
@@ -56,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         webview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if(url.startsWith("tel:")) {
+                if (url.startsWith("tel:")) {
                     Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
                     startActivity(intent);
                     view.reload();
@@ -124,14 +142,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK) {
-            if(webview.canGoBack()) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webview.canGoBack()) {
                 webview.goBack();
                 return false;
             }
         }
 
-        if(keyCode == KeyEvent.KEYCODE_BACK && !webview.canGoBack()) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && !webview.canGoBack()) {
             new AlertDialog.Builder(this)
                     .setTitle("프로그램 종료")
                     .setMessage("모두의 잔을 종료하시겠습니까?")
@@ -141,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
                             android.os.Process.killProcess(android.os.Process.myPid());
                         }
                     })
-                    .setNegativeButton("아니오",null)
+                    .setNegativeButton("아니오", null)
                     .show();
         }
 
@@ -152,11 +170,11 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == 1) {
-            if(grantResults.length > 0) {
-                for(int i=0; i < grantResults.length; i++) {
-                    if(grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                        //위치 권한 팝업을 띄움
+        if (requestCode == 1) {
+            if (grantResults.length > 0) {
+                for (int i = 0; i < grantResults.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        // 위치 권한 팝업을 띄움
                         new AlertDialog.Builder(this).setMessage("위치 권한을 허용해주세요")
                                 .setPositiveButton("종료", new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
@@ -175,8 +193,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                 })
                                 .setCancelable(false).show();
-                    }
-                    else {
+                    } else {
                         setWebView();
                     }
                 }
@@ -184,5 +201,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static class MyBottomSheetDialogFragment extends BottomSheetDialogFragment {
+        private boolean waitingForDismissAllowingStateLoss;
 
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            return new BottomSheetDialog(requireContext(), getTheme());
+        }
+
+        @Override
+        public void dismiss() {
+            if (!tryDismissWithAnimation(false)) {
+                super.dismiss();
+            }
+        }
+
+        @Override
+        public void dismissAllowingStateLoss() {
+            if (!tryDismissWithAnimation(true)) {
+                super.dismissAllowingStateLoss();
+            }
+        }
+
+        private boolean tryDismissWithAnimation(boolean allowingStateLoss) {
+            Dialog baseDialog = getDialog();
+            if (baseDialog instanceof BottomSheetDialog) {
+                BottomSheetDialog dialog = (BottomSheetDialog) baseDialog;
+                BottomSheetBehavior<?> behavior = dialog.getBehavior();
+                if (behavior.isHideable() && dialog.getDismissWithAnimation()) {
+                    dismissWithAnimation(behavior, allowingStateLoss);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void dismissWithAnimation(@NonNull BottomSheetBehavior<?> behavior, boolean allowingStateLoss) {
+            waitingForDismissAllowingStateLoss = allowingStateLoss;
+
+            if (behavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                dismissAfterAnimation();
+            } else {
+                behavior.addBottomSheetCallback(new BottomSheetDismissCallback());
+                behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        }
+
+        private void dismissAfterAnimation() {
+            if (waitingForDismissAllowingStateLoss) {
+                super.dismissAllowingStateLoss();
+            } else {
+                super.dismiss();
+            }
+        }
+
+        private class BottomSheetDismissCallback extends BottomSheetBehavior.BottomSheetCallback {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    dismissAfterAnimation();
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
+        }
+    }
 }

738cee42ff822f1ae9f1625c5bcad4facbec87dc
haa741123 <ihamin012@gmail.com>
2024-06-13 22:40:41 +0900

    스플래시 화면 디자인 업데이트, 회원가입 페이지 클릭 시 로그인 페이지 이동 로그인 페이지 추가

---
+++ app/src/main/java/com/example/test/Login.java
@@ -0,0 +1,96 @@
+package com.example.test;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class Login extends AppCompatActivity {
+    WebView webview;
+    TextView textview;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        webview = findViewById(R.id.activity_main_web2);
+
+        setWebView();  // WebView 초기화를 참조 뒤에 호출
+
+    }
+
+    private void setWebView() {
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("tel:")) {
+                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(url)));
+                    view.reload();
+                    return true;
+                }
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                webview.setVisibility(View.GONE);
+                textview.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> result.confirm())
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+        });
+
+        webview.loadUrl("http://121.142.17.86:80/login");
+    }
+
+
+}
--- app/src/main/java/com/example/test/Splash.java
+++ app/src/main/java/com/example/test/Splash.java
@@ -14,17 +14,7 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        // TextView와 Button 찾기
-        TextView skip = findViewById(R.id.textView);
         TextView next = findViewById(R.id.textView2);
-
-        // TextView 클릭 리스너 설정
-        skip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moveMain();
-            }
-        });
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +26,7 @@ public class Splash extends AppCompatActivity {
 
     // 메인 액티비티로 이동하는 메소드
     private void moveMain() {
-        Intent intent = new Intent(Splash.this, MainActivity.class);
+        Intent intent = new Intent(Splash.this, Login.class);
         startActivity(intent);
         finish(); // 현재 액티비티 종료
     }
